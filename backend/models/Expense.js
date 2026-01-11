946d8d41d5a105ec61df96ceb82da8027a6e0926
Yeshwanth <vryeshwanth06@gmail.com>
2026-01-11 20:28:39 +0530

    Add User and Expense models

---
+++ backend/models/Expense.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const ExpenseSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        title: { type: String, required: true },
+        amount: { type: Number, required: true },
+        category: { type: String, required: true },
+        date: { type: Date, required: true },
+        description: { type: String },
+    },
+    { timestamps: true }
+);
+
+const Expense = mongoose.model("Expense", ExpenseSchema);
+
+export default Expense;
