4362076c5479a7d7d77649b770327b9cbfb4f04b
Yeshwanth <vryeshwanth06@gmail.com>
2026-01-13 18:17:59 +0530

    Add middleware to verify token

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async (req, res, next) => {
+    const header = req.headers["Authorization"];
+    const token = header && header.split(" ")[1];
+
+    if (!token) return res.status(404).json({ error: "Access Denied !!" });
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(400).json({ error: "Invalid or expired token !!" });
+    }
+};
